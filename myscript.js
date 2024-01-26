ba6556a07955d2475f7834f5d7e29a904b3b9986
Newb480 <156213267+Newb480@users.noreply.github.com>
2024-01-26 16:07:37 +0500

    threejs

--- myscript.js
+++ myscript.js
@@ -1,3 +1,18 @@
-window.onload = function() {
-    alert("Hello, World!");
+import * as THREE from 'three/build/three.module.js'
+import { GLTFLoader } from 'three/jsm/loaders/GLTFLoader'
+
+const scene: THREE.Scene = new THREE.Scene ()
+
+const loader = new GLTFLoader ()
+loader.load ('three/examples/models/gltf/BoomBox.glb', function (gltf) {
+    scene.add (gltf.scene);
+    gltf.scene.position.set (0,-2,3)
+}, (xhr) => {
+    console.log ((xhr.loaded / xhr.total * 100) + '% loaded')
+}, (error) => {
+    console.log (error);
+});
+
+function render () {
+    renderer.render (scene, camera)
 }
